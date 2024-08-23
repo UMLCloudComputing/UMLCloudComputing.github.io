481f828a063232fe98e721616c3937d71556a0c3
Gurpranked <gurpreet.singh.rehal@outlook.com>
2024-08-23 16:06:29 -0400

    feat: Added member avatar component with modal functionality on landing page.

---
+++ src/components/AvatarLarge.tsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import Link from "@docusaurus/Link";
+import { Backdrop, Modal, Fade, Box } from "@mui/material";
+
+function MemberCard ({ ProfileImage, Name, Subtitle, Details, Github }) {
+    return (
+        <>
+            <div className="card__header">
+                <div className="avatar avatar--vertical">
+                    <img 
+                        className='avatar__photo avatar__photo--xl'
+                        src={ProfileImage} 
+                    />
+                    <div className="avatar__intro">
+                        <div className="avatar__name">{Name}</div>
+                        <small className="avatar__subtitle">{Subtitle}</small>
+                    </div>
+                </div>
+            </div>
+            <div className="card__body">
+                <small>
+                    {Details}
+                </small>
+            </div>
+            <div className="card__footer">
+                <Link className="button button--secondary button--block" to={Github}>Github</Link>
+            </div>
+        </>
+    );
+}
+
+
+function AvatarLarge({ member }) {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+    return (
+    <div>
+        <div className="avatar" onClick={handleOpen}>
+            <img 
+                className="avatar__photo avatar__photo--lg"
+                src={member.profile_image} />
+            <div className="avatar_intro">
+                <div className="avatar__name"> {member.name} </div>
+                <small className="avatar__subtitle"> {member.subtitle} </small>
+            </div>
+        </div>
+        <Modal
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+            open={open}
+            disableScrollLock
+            onClose={handleClose}
+            closeAfterTransition
+            style={{ backdropFilter: 'blur(5px)' }}
+            slots={{ backdrop: Backdrop }}
+            slotProps={{
+                backdrop: {
+                    timeout: 500,
+                },
+            }}
+        >
+            <Fade in={open}>
+                <Box className="card" sx={{
+                    position:'absolute',
+                    top: '50%',
+                    left:'50%',
+                    transform: 'translate(-50%, -50%)',
+                    boxShadow: 25,
+                 }}>
+                    <MemberCard
+                        ProfileImage={member.profile_image}
+                        Name={member.name}
+                        Subtitle={member.subtitle}
+                        Details={member.details}
+                        Github={member.github}  
+                    />
+                </Box>
+            </Fade>
+        </Modal>
+    </div>
+    );
+};
+
+export default AvatarLarge;
