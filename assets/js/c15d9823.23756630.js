d4bfe763489ba2d5a10d456efa5b7c1a434a0073
Gurpranked <Gurpranked@users.noreply.github.com>
2024-09-02 02:16:18 +0000

    deploy: 4f1bbb2cd5faa06dc329d483722e3a8f287c95ea

--- assets/js/c15d9823.23756630.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkumlcloudcomputing=self.webpackChunkumlcloudcomputing||[]).push([[8146],{9328:l=>{l.exports=JSON.parse('{"metadata":{"permalink":"/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":3,"blogDescription":"The Offical Blog page of UML Cloud Computing Club","blogTitle":"Blog"}}')}}]);
