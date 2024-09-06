bcce470e02f0646c1e9e7bfc62aa3e577973dc9d
Gurpranked <gurpreet.singh.rehal@outlook.com>
2024-09-05 20:08:08 -0400

    feat: Added activities remote docs functionality

--- stream.js
+++ stream.js
@@ -2,11 +2,41 @@ const { execSync } = require('child_process');
 const { readFileSync, mkdirSync, existsSync, writeFileSync } = require('fs');
 const { join } = require('path');
 
-const repos = JSON.parse(readFileSync('repos.json', 'utf8'));
+const project_repos = JSON.parse(readFileSync('project_repos.json', 'utf8'));
+const activity_repos = JSON.parse(readFileSync('activity_repos.json', 'utf8'))
 position = 2;
 
-for (const [name, url] of Object.entries(repos)) {
-  const repoDir = join(__dirname, 'temp', name);
+for (const [name, url] of Object.entries(activity_repos)) {
+  const repoDir = join(__dirname, 'temp_activity', name);
+  const destDir = join(__dirname, 'docs', 'activities', name);
+
+  // Clone the repository
+  execSync(`git clone ${url} ${repoDir}`);
+
+  // Create destination directory if it doesn't exist
+  if (!existsSync(destDir)) {
+    mkdirSync(destDir, { recursive: true });
+  }
+
+  // Copy the docs/web_docs directory to the destination
+  execSync(`cp -r ${join(repoDir, 'docs', 'web_docs')}/* ${destDir}`);
+
+  // Create _category_.json file
+  const categoryContent = {
+    label: name,
+    position: position,
+    link: {
+      type: "generated-index"
+    }
+  };
+  writeFileSync(join(destDir, '_category_.json'), JSON.stringify(categoryContent, null, 2));
+
+  console.log(`Copied docs from ${name}`);
+  position++;
+}
+
+for (const [name, url] of Object.entries(project_repos)) {
+  const repoDir = join(__dirname, 'temp_projects', name);
   const destDir = join(__dirname, 'docs', 'projects', name);
 
   // Clone the repository
